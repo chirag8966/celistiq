cc44019de73b37155a8bf8bbaa903ff6c6ca62f9
chirag jain <chirag8966@gmail.com>
2019-05-21 17:58:55 +0530

    contacts added to be fetched

--- phptasks/login.php
+++ phptasks/login.php
@@ -32,7 +32,9 @@ if (!$db_select) {
 	echo "<table>"; // start a table tag in the HTML
 
 	while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results
-	echo "<tr><td>" . $row['username'] . "</td><td>" . $row['email'] . "</td></tr>";  //$row['index'] the index here is a field name
+	echo "<tr><td>" .$row['username'] . "</td><td>" .$row['email'] . "</td><td>" .$row['contact'] . "</td></tr>";  
+
+	//$row['index'] the index here is a field name
 	}
 
 	echo "</table>"; //Close the table in HTML
